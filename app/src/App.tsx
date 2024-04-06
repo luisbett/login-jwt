d017a8fb128b13f3fa26d8e3292fc04360b6aa2d
Luis Fellipy Bett <luisfbett@gmail.com>
2024-04-06 12:08:37 +0100

    change route definition to App component

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,13 +1,33 @@
-import { Outlet } from 'react-router-dom'
-import classes from './App.module.css'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+
 import { Toaster } from 'react-hot-toast'
 
+import SignIn from './routes/SignIn'
+import SignUp from './routes/SignUp'
+import Home from './routes/Home'
+import NotFound from './routes/NotFound'
+
+import classes from './App.module.css'
+
 function App() {
+
+  //Get token
+  const token = localStorage.getItem('token')
+  
+  //If token exists, check if it is a valid token
+  const validToken = token //&& await useToken(token)
 
   return (
     <div className={classes.container}>
       <Toaster />
-      <Outlet />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={validToken ? <Navigate to="/home" replace /> : <SignIn />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/home' element={validToken ? <Home /> : <Navigate to="/" replace />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,54 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom'
 
 import { UserProvider } from './contexts/UserContext.tsx'
 
-import useToken from './hooks/useToken.ts'
-
 import App from './App.tsx'
-import Home from './routes/Home.tsx'
-import SignIn from './routes/SignIn.tsx'
-import SignUp from './routes/SignUp.tsx'
-import NotFound from './routes/NotFound.tsx'
 
 import './index.css'
-
-//Get token
-const token = localStorage.getItem('token')
-
-//If token exists, check if it is a valid token
-const validToken = token && await useToken(token)
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        path: '/',
-        element: validToken ? <Navigate to="/home" replace /> : <SignIn />,
-      },
-      {
-        path: '/signup',
-        element: <SignUp />
-      },
-      {
-        path: '/home',
-        element: validToken ? <Home /> : <Navigate to="/" replace />
-      },
-      {
-        path: '*',
-        element: <NotFound />
-      }
-    ]
-  }
-])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <UserProvider>
-      <RouterProvider router={router} />
+      <App />
     </UserProvider>
   </React.StrictMode>,
 )
