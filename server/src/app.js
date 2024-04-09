e5c9dc06122fa671aa2001a975c96d0ff3d5c1a4
Luis Fellipy Bett <luisfbett@gmail.com>
2024-04-09 13:42:13 +0100

    change cors definition to app file and add cookie-parser package

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,20 @@
 const express = require('express')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
 
 const router = require('./router')
 
 const app = express()
 
+app.use(
+    cors({
+        credentials: true,
+        origin: 'http://localhost:5173'
+    })
+)
+
 app.use(express.json())
+app.use(cookieParser())
 app.use(router)
 
 module.exports = app
--- server/src/router.js
+++ server/src/router.js
@@ -2,17 +2,8 @@ const express = require('express')
 
 const router = express.Router()
 
-const cors = require('cors')
-
 const usersController = require('./controllers/usersController')
 const usersMiddleware = require('./middlewares/usersMiddleware')
-
-router.use(
-    cors({
-        credentials: true,
-        origin: 'http://localhost:5173'
-    })
-)
 
 router.post('/auth/token', usersMiddleware.verifyToken)
 router.post('/auth/user', usersMiddleware.validateLogin)
